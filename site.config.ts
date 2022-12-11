7e8ab9831387455a853c4fbb98c6270fdc2b4f8b
sijia-w <53718687+sijia-w@users.noreply.github.com>
2022-12-11 19:36:34 +0100

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,16 +2,16 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'Language-Tailors-8f1edc71772c42b28b8f8cc2f69ba43e',
+  rootNotionPageId: '8f1edc71772c42b28b8f8cc2f69ba43e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Language Tailors',
-  domain: 'language-tailors.it',
-  author: 'Language Tailors',
+  name: 'Language Tailors Studio',
+  domain: 'language-tailors-studio',
+  author: 'Language Tailors Studio',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
